046e15c59f0a30dd09d0f14452d70e1ce7df068f
Stas Kelvich <stanconn@gmail.com>
2017-11-24 15:25:20 +0300

    support for referee node in reinit-mm.sh

--- tests/reinit-mm.sh
+++ tests/reinit-mm.sh
@@ -1,9 +1,10 @@
 CURPATH=`pwd`
+USER=`whoami`
 BASEDIR=$CURPATH/../../..
 export PATH=$BASEDIR/tmp_install/usr/local/pgsql/bin/:$PATH
 export DESTDIR=$BASEDIR/tmp_install
 
-n_nodes=3
+n_nodes=2
 ulimit -c unlimited
 pkill -9 postgres
 
@@ -13,21 +14,37 @@ make install
 cd $BASEDIR/contrib/mmts
 make clean && make install
 
+cd $BASEDIR/contrib/referee
+make clean && make install
+
 cd $BASEDIR/contrib/mmts/tests
 
-rm -fr node? *.log
+rm -rf referee node? *.log
+
+###############################################################################
+
+initdb referee
+cat <<SQL > referee/postgresql.conf
+    listen_addresses='*'
+    port = '5440'
+SQL
+pg_ctl -w -D referee -l referee.log start
+createdb -p 5440
+psql -p 5440 -c 'create extension referee'
+
+###############################################################################
+
 conn_str=""
 sep=""
 for ((i=1;i<=n_nodes;i++))
 do    
     port=$((5431 + i))
     arbiter_port=$((7000 + i))
-    conn_str="$conn_str${sep}dbname=regression user=stas host=127.0.0.1 port=$port arbiter_port=$arbiter_port sslmode=disable"
+    conn_str="$conn_str${sep} dbname=$USER host=127.0.0.1 port=$port arbiter_port=$arbiter_port sslmode=disable"
     sep=","
     initdb node$i
     pg_ctl -w -D node$i -l node$i.log start
-    createdb regression
-    # psql regression -c "create table t(id int primary key, v int); insert into t values($i,$i);"
+    createdb
     pg_ctl -w -D node$i -l node$i.log stop
 done
 
@@ -42,33 +59,29 @@ do
     cat <<SQL > node$i/postgresql.conf
         listen_addresses='*'
         port = '$port'
-        max_prepared_transactions = 100
-        synchronous_commit = on
+        max_prepared_transactions = 300
         fsync = on
         wal_level = logical
-        max_worker_processes = 100
+        max_worker_processes = 300
         max_replication_slots = 10
         max_wal_senders = 10
         shared_preload_libraries = 'multimaster'
-        default_transaction_isolation = 'repeatable read'
+        wal_writer_delay = 5ms
 
-        multimaster.workers = 1
         multimaster.heartbeat_recv_timeout = 2000
         multimaster.heartbeat_send_timeout = 250
-        #multimaster.volkswagen_mode = 1
         multimaster.conn_strings = '$conn_str'
         multimaster.node_id = $i
-        multimaster.max_nodes = 4
         multimaster.arbiter_port = $arbiter_port
-        multimaster.min_2pc_timeout = 10000000
-        multimaster.trans_spill_threshold = 100
-        multimaster.monotonic_sequences = true
+        multimaster.max_recovery_lag = 30GB
+        multimaster.referee_connstring = 'dbname=$USER host=127.0.0.1 port=5440 sslmode=disable'
 SQL
     cp pg_hba.conf node$i
     pg_ctl -w -D node$i -l node$i.log start
 done
 
 sleep 10
-psql regression < regress.sql
+psql < regress.sql
+psql -c 'create extension multimaster'
 
 echo Done
