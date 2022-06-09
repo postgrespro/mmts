54456baab67dd4c63af42a570ab01afab77831db
Mikhail Rutman <m.rutman@postgrespro.ru>
2022-06-09 23:32:50 +0700

    Enable TAP test atx2
    Tags: Multimaster

--- t/001_regress.pl
+++ t/001_regress.pl
@@ -86,7 +86,6 @@ $schedule =~ s/test: cfs/#test: cfs/g;
 $schedule =~ s/test: largeobject//; # serial schedule
 $schedule =~ s/largeobject//; # parallel schedule
 $schedule =~ s/atx0//; # parallel schedule
-$schedule =~ s/atx2//; # parallel schedule
 unlink('parallel_schedule');
 TestLib::append_to_file('parallel_schedule', $schedule);
 
